a4b808796c567aa954adce68d558735157f6c7fc
Đỗ Minh Thuận <dominhthuan17102005@gmail.com>
2026-05-08 00:20:24 +0700

    update

--- src/main/java/service/BanService.java
+++ src/main/java/service/BanService.java
@@ -7,4 +7,7 @@ public interface BanService {
     List<Ban> findAll();
     Ban findById(String maBan);
     boolean updateStatus(String maBan, String status);
+    boolean addBan(Ban ban);
+    boolean updateBan(Ban ban);
+    boolean deleteBan(String maBan);
 }
